7077feb3c081c330b190b6f09ab2426c70b0818a
jackdotwa <jboydutoit@gmail.com>
2019-04-08 21:44:16 +0200

    added more tests to person embeddingg

--- tests/test_embedding_person.py
+++ tests/test_embedding_person.py
@@ -1,5 +1,7 @@
 import unittest
 import embedding_person as ep
+from unittest import mock
+from unittest.mock import MagicMock
 
 
 class TestProcessorP1(unittest.TestCase):
@@ -15,3 +17,20 @@ class TestProcessorP1(unittest.TestCase):
         spouses = ep._get_spouses(parents, current_parent, person_record)
         self.assertListEqual(spouses, [parents[1]])
 
+        with self.assertRaises(Exception) as context:
+            ep._get_spouses(['A', 'B', 'C'], 'D', person_record)
+
+
+    # todo this test is not satisfactory - must be integration test
+    @mock.patch('pymongo.collection.Collection')
+    def test_update_children(self, coll):
+        coll.find_one = MagicMock()
+        coll.find_one.return_value = []
+        parents = ['A', 'B']
+        children = ['a', 'b']
+        # todo modify the code to separate the mongo write so unittest easier
+        # we can check what the mongo function was called with, but that seems
+        ep._update_children(coll, parents, children)
+        # other conditional
+        coll.find_one.return_value = ['C']
+        ep._update_children(coll, parents, children)
