55a01c3fac69a6229bbd854d6e602fd20c0588a4
jackdotwa <jacques@spatialedge.co.za>
2019-04-28 11:41:08 +0200

    broke out a private function to make testing of generator in ingest.read() easier. added initial tests

--- ingest.py
+++ ingest.py
@@ -1,7 +1,10 @@
 import elements
 import pprint
 from dbinterface import *
+import logging
+from typing import Iterator
 log = logging.getLogger(__name__)
+print(logging.getLogger())
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -26,10 +29,34 @@ class FileIngestor(object):
         # todo can get the collection name out here and use it for the metacount
         #self.meta_data['count'] += 1
 
-    def read(self):
+    def _buffered_read(self, filehandler, buffered_lines):
+        """
+        A helper function to buffer the lines. It is operator on the references so it is changing the
+        filehandler and the buffered_lines data structure.
+        :param filehandler:
+        :param buffered_lines:
+        :return:
+        """
+        while True:
+            last_pos = filehandler.tell()
+            line = filehandler.readline()
+            if not line:  # readline can return None if the file ends unexpectedly
+                break
+            line = line.lstrip(' ').lstrip('\n')
+            if line and '0' in line[0]:
+                # must deal sequentially due to max recursion depth
+                filehandler.seek(last_pos)
+                log.debug('full record passed to parser is: {}'.format(buffered_lines))
+                break
+            else:  # int(line[0]) > 0
+                log.debug('appending... {}'.format(line))
+                buffered_lines.append(line)
+
+    def read(self) -> Iterator[tuple]:
         """
         parse a .ged formatted file by reading it line
         for line and interpreting the leftmost code and keys.
+        This is a generator function
         :return:
         """
         with open(self.filename, 'r',  encoding='utf-8-sig') as f:
@@ -37,36 +64,34 @@ class FileIngestor(object):
             while line != "":
                 # strip away leading whitespace
                 line = line.lstrip(' ')
+                print(line)
+                print(log)
                 if line and line[0] == '0':
-                    if line[0] == 'TRLR':
-                        # termination symbol
+                    if line.startswith('0 TRLR'):
+                        # termination symbol - explicitly terminate
                         log.info('file parsing complete\nStats:')
+                        raise StopIteration('completed file parsing')
                     else:
                         # start buffering lines until the next 0
+                        print('--here')
                         buffered_lines = [line]
-                        while True:
-                            last_pos = f.tell()
-                            line = f.readline()
-                            line = line.lstrip(' ').lstrip('\n')
-                            if not line:
-                                # the jig is up
-                                break
-                            elif line and '0' in line[0]:
-                                # must deal sequentially due to max recursion depth
-                                f.seek(last_pos)
-                                log.debug('full record passed to parser is: {}'.format(buffered_lines))
-                                break
-                            elif int(line[0]) > 0:
-                                log.debug('appending... {}'.format(line))
-                                buffered_lines.append(line)
+                        print('-- {}'.format(buffered_lines))
+                        self._buffered_read(f, buffered_lines)
                         # done buffering
                         if elements.Family.is_family(buffered_lines[0]):
                             data_dict = elements.Family(buffered_lines).parser()
+                            print("family done")
                             yield 'family', data_dict
                         elif elements.Person.is_person(buffered_lines[0]):
                             data_dict = elements.Person(buffered_lines).parser()
                             print('person done {}'.format(buffered_lines[0]))
                             yield 'person', data_dict
+                        else:
+                            print(buffered_lines)
+                            print("xxxxxxxxxxxxxxxxxxx")
+                            log.debug('this non-breaking functionality is not implemented')
+                else:
+                    raise RuntimeError('gedcom format not as expected, or buffering of reads is failing')
                 line = f.readline()
 
     def ingest(self, *args, **kwargs):
--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -1,7 +1,8 @@
 import unittest
 import embedding_person as ep
 from unittest import mock
-from unittest.mock import MagicMock, call
+
+from unittest.mock import MagicMock, call, mock_open, patch
 import ingest
 
 
@@ -30,3 +31,52 @@ class TestIngest(unittest.TestCase):
         io.read.return_value = iter([('person', [1, 2, 3])])
         io.write = MagicMock()
         io.ingest(person=read_mock, family='family')
+
+    @patch("builtins.open", new_callable=mock_open, read_data="")
+    def test_ingest_read_with_nothing(self, mock_open):
+        io = ingest.FileIngestor('nf')
+        with self.assertRaises(StopIteration) as context:
+            next(io.read())
+
+    @patch("builtins.open", new_callable=mock_open, read_data="0 TRLR stuff")
+    @patch('ingest.log')
+    def test_ingest_read_at_trlr(self, mock_log, mock_open):
+        io = ingest.FileIngestor('nf')
+        with self.assertRaises(StopIteration) as context:
+            next(io.read())
+        expected_calls_to_logger = [
+            call('file parsing complete\nStats:')
+        ]
+        #print(mock_log.info.call_args_list)
+        mock_log.info.assert_has_calls(expected_calls_to_logger, any_order=True)
+
+    def test_private_buffered_read(self):
+        filehandler = MagicMock()
+        filehandler.tell.side_effect = [10, 11, 12, 13]  # same length as readline
+        lines = ['1 line 1', '2 line 2', '3 line 3', '0 end']
+        filehandler.readline.side_effect = lines
+        io = ingest.FileIngestor('nf')
+        buffered_lines = ['start']
+        io._buffered_read(filehandler, buffered_lines)
+        self.assertListEqual(['start'] + lines[:-1], ['start', '1 line 1', '2 line 2', '3 line 3'])
+
+    def test_private_buffered_read_conditionals(self):
+        # test line being none conditional
+        filehandler = MagicMock()
+        buffered_lines = ['start']
+        filehandler.tell.side_effect = [10, 11, 12, 13]  # same length as readline
+        filehandler.readline.return_value = None
+        io = ingest.FileIngestor('nf')
+        io._buffered_read(filehandler, buffered_lines)
+        self.assertListEqual(['start'], buffered_lines)
+
+        # deal with case where
+
+
+
+    # @patch("builtins.open", new_callable=mock_open, read_data="0 @I1@\n1 LINE1\n0 STUFF")
+    # @patch('ingest.log')
+    # @patch('elements.Person') # you are here
+    # def test_ingest_read_at_trlr(self, mock_log, mock_open):
+    #     io = ingest.FileIngestor('nf')
+    #     next(io.read())
