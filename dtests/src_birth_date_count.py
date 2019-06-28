2902662c3e811458a8120ce71fa2c45f3cded75f
jackdotwa <jacques@spatialedge.co.za>
2019-06-29 01:46:55 +0200

    tracked double birth bug and confirmed that data validates correctly - added birth count to person script

---
+++ dtests/src_birth_date_count.py
@@ -0,0 +1,25 @@
+
+import argparse
+import re
+
+
+def get_indi(filename):
+    indi_list = []
+    with open(filename, 'r', encoding='utf-8-sig') as f:
+        for line in f:
+            if bool(re.search('0 @[A-Z0-9]*@ INDI', line)):
+                indi = line
+            if bool(re.search('^1 BIRT$', line)):
+                line = f.readline()
+                if bool(re.search('^2 DATE', line)):
+                    indi_list.append(indi.replace('\n', ''))
+    return indi_list
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('test')
+    parser.add_argument('-f', '--file', required=True, help="gedcom file")
+
+    args = parser.parse_args()
+    print('{}'.format('\n'.join(get_indi(args.file))))
+
